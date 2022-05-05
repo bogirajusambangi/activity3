79a4153b316b063e9fdcf63afb685d98d475ec37
bogirajusambangi <98826655+bogirajusambangi@users.noreply.github.com>
2022-05-05 12:44:03 +0530

    Update name.c

--- name.c
+++ name.c
@@ -1,16 +1,16 @@
 #include"fun.h"
 #include<string.h>
 #include<stdio.h>
-int name(char a[])
-{  char *p=a;
-   int sum =0;
-   int b=strlen(a);
-   int arr[b];
-for(int i=0;i<=b;i++)
+int name(char a[])    // function definition
+{  char *p=a;         //assignin pointer to char array
+   int sum =0;        // taking sum var =0
+   int b=strlen(a);   // storing string length
+   int arr[b];        // creating integer array based on char array length
+for(int i=0;i<=b;i++)  // running for loop to type casting and adding array elements
 {
-    arr[i]=(int)*(p+i);
-    sum+=arr[i];
+    arr[i]=(int)*(p+i);  //typecasting the each element of char array and storing in integer array
+    sum+=arr[i];        // summing all elements in array
 }    
-    return sum;
+    return sum;        // returning sum
 
 }
